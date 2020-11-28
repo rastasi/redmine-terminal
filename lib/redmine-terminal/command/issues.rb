f7cad332a069b33299a8c016b4cd638dfd76efeb
Zsolt Tasnadi <rastasi@gmail.com>
2020-11-28 11:59:01 +0100

    assigned_to fix

--- lib/redmine-terminal/command/issues.rb
+++ lib/redmine-terminal/command/issues.rb
@@ -14,7 +14,7 @@ RedmineTerminal::Command.new :issues do
     line.push "#"+issue['id'].to_s
     line.push issue['project']['name']
     line.push issue['subject']
-    line.push issue['assigned_to']['name'] if ARGV[1] != 'me'
+    line.push issue['assigned_to']['name'] if ARGV[1] != 'me' and issue['assigned_to'] != nil
     lines.push line
   end
 
